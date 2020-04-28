8cf82fd703bffb7871740022a8a5ded573080535
Nick Brassel <nick@tzarc.org>
2020-04-29 04:48:23 +1000

    The great licensing fiasco of 2020.

--- build_all.sh
+++ build_all.sh
@@ -3,10 +3,11 @@
 this_script=$(readlink -f "${BASH_SOURCE[@]}")
 script_dir=$(dirname "$this_script")
 
+export PATH=/home/nickb/gcc-arm/gcc-arm-none-eabi-9-2019-q4-major/bin:$PATH
 MAKEFLAGS="-j --output-sync"
 
 pushd "$script_dir/qmk_firmware" >/dev/null 2>&1 \
-    && time make $MAKEFLAGS all:default MAKEFLAGS="$MAKEFLAGS" 2>&1 | tee "$script_dir/build.log" | egrep '\[(ERRORS|WARNINGS|OK)\]' \
+    && time make $MAKEFLAGS all:default MAKEFLAGS="$MAKEFLAGS" "$@" 2>&1 | tee "$script_dir/build.log" | egrep '\[(ERRORS|WARNINGS|OK)\]' \
     && popd >/dev/null 2>&1
 
 num_successes=$(cat "$script_dir/build.log" | egrep '\[(OK)\]' | wc -l)
