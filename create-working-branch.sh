aedbacb5e2f9ebe15365d73219aab76f68b1c666
Nick Brassel <nick@tzarc.org>
2020-10-23 15:57:16 +1100

    gcc10, Djinn conf migration.

--- create-working-branch.sh
+++ create-working-branch.sh
@@ -19,15 +19,12 @@ fi
 
 declare -a prs_to_apply
 prs_to_apply+=(9603) # Matrix delay
+prs_to_apply+=(10491) # Atomic GPIO ops
 prs_to_apply+=(10088) # ChibiOS USB Wakeup
 prs_to_apply+=(10174) # Quantum Painter
-prs_to_apply+=(10260) # Backlight limit
 prs_to_apply+=(10418) # ChibiOS conf upgrade
 prs_to_apply+=(10437) # Decouple USB events
 prs_to_apply+=(10530) # housekeeping
-prs_to_apply+=(10638) # RGB hooks
-prs_to_apply+=(10716) # Serial driver #define
-prs_to_apply+=(10491) # Atomic GPIO ops
 
 declare -a cherry_picks
 cherry_picks+=(ba542a0164a10a1958aa5054d2a0cc333bb3ce64) # Audio
