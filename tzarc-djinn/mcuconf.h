aa9b8d37b95f5e63d98b40662226b91c00cb0cae
Nick Brassel <nick@tzarc.org>
2021-02-09 13:41:43 +1100

    Disable GPT 6/7/8, as audio runs off PWM1.

--- tzarc-djinn/mcuconf.h
+++ tzarc-djinn/mcuconf.h
@@ -18,27 +18,23 @@
 
 #include_next <mcuconf.h>
 
-#undef STM32_GPT_USE_TIM6
-#define STM32_GPT_USE_TIM6 TRUE
-
-#undef STM32_GPT_USE_TIM7
-#define STM32_GPT_USE_TIM7 TRUE
-
-#undef STM32_GPT_USE_TIM8
-#define STM32_GPT_USE_TIM8 TRUE
-
+// Used for audio
 #undef STM32_PWM_USE_TIM1
 #define STM32_PWM_USE_TIM1 TRUE
 
+// Used for backlight
 #undef STM32_PWM_USE_TIM17
 #define STM32_PWM_USE_TIM17 TRUE
 
+// Used for SK6812 chain
 #undef STM32_PWM_USE_TIM20
 #define STM32_PWM_USE_TIM20 TRUE
 
+// Used for split comms
 #undef STM32_SERIAL_USE_USART3
 #define STM32_SERIAL_USE_USART3 TRUE
 
+// Used for EEPROM
 #undef STM32_SPI_USE_SPI3
 #define STM32_SPI_USE_SPI3 TRUE
 
