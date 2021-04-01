111a24a25b9b15df3026dec61dcde816e1939739
Nick Brassel <nick@tzarc.org>
2021-04-01 13:33:10 +1100

    Reworked build-all script.

--- build_all.sh
+++ build_all.sh
@@ -1,28 +1,86 @@
 #!/bin/bash
 
+set -e
+
 this_script=$(readlink -f "${BASH_SOURCE[@]}")
 script_dir=$(dirname "$this_script")
+qmk_firmware="$script_dir/qmk_firmware"
+qmk_builddir="$qmk_firmware/.build"
+temp_makefile="$qmk_builddir/parallel_kb_builds.mk"
 
-#export PATH=/home/nickb/gcc-arm/gcc-arm-none-eabi-8-2018-q4-major/bin:$PATH
+unset NO_CI
+[[ "$1" == "noci" ]] && NO_CI="noci"
+
 export PATH=/usr/lib/ccache:$PATH
-MAKEFLAGS="-j --output-sync"
+MAKEFLAGS="-j$(( $(nproc) * 2 + 1 )) --output-sync --no-print-directory"
 
-pushd "$script_dir/qmk_firmware" >/dev/null 2>&1 \
-    && time remake $MAKEFLAGS all:default "$@" 2>&1 | tee "$script_dir/build.log" | egrep '\[(ERRORS|WARNINGS|OK)\]' \
-    && popd >/dev/null 2>&1
+declare -A all_keyboards=()
 
-#pushd "$script_dir/qmk_firmware" >/dev/null 2>&1 \
-#    && time { ./util/list_keyboards.sh | sort | uniq | parallel --eta --bar --plain --group --keep-order --retries 3 --jobs 200% remake -j4 -O {}:default SILENT=true REQUIRE_PLATFORM_KEY=chibios ; } | tee "$script_dir/build.log" | egrep '\[(ERRORS|WARNINGS|OK)\]' \
-#    && popd >/dev/null 2>&1
+determine_keyboards() {
+	pushd "$qmk_firmware" >/dev/null 2>&1
+	for kb in $(./util/list_keyboards.sh ${NO_CI:-}) ; do
+		kbpath="$kb"
+		if [[ -f "keyboards/$kb/rules.mk" ]] ; then
+			default_folder=$(grep DEFAULT_FOLDER "keyboards/$kb/rules.mk" | cut -d'=' -f2 | xargs echo)
+			if [[ -n "$default_folder" ]] ; then
+				kbpath="$default_folder"
+			fi
+		fi
+		all_keyboards[$kbpath]=1
+	done
+	popd >/dev/null 2>&1
+}
 
-num_successes=$(cat "$script_dir/build.log" | egrep '\[(OK)\]' | wc -l)
-num_skipped=$(cat "$script_dir/build.log" | egrep '\[(SKIPPED)\]' | wc -l)
-num_warnings=$(cat "$script_dir/build.log" | egrep '\[(WARNINGS)\]' | wc -l)
-num_failures=$(cat "$script_dir/build.log" | egrep '\[(ERRORS)\]' | wc -l)
-echo "-------------------------------"
-echo "Successful builds: $num_successes"
-echo "Skipped builds: $num_skipped"
-echo "Warning builds: $num_warnings"
-echo "Failing builds: $num_failures"
-echo "-------------------------------"
-cat "$script_dir/build.log" | egrep '\[(ERRORS)\]'
+dump_keyboard_list() {
+	for kb in ${!all_keyboards[@]} ; do
+		echo $kb
+	done | sort | uniq
+}
+
+generate_makefile() {
+	pushd "$qmk_firmware" >/dev/null 2>&1
+	truncate -s 0 "$temp_makefile"
+	for kb in $(dump_keyboard_list) ; do
+		safe=$(echo $kb | sed -e 's@/@_@g')
+		buildfile="$qmk_builddir/build.log.${safe}"
+		failfile="$qmk_builddir/failed.log.${safe}"
+		cat << EOF >> "$temp_makefile"
+all: ${safe}_binary
+${safe}_binary:
+	@rm -f "$failfile" || true
+	@printf '^^keyboard=${kb}\n^^safe=${safe}\n' >"$buildfile"
+	+@\$(MAKE) -C "$qmk_firmware" -f "$qmk_firmware/build_keyboard.mk" KEYBOARD="$kb" KEYMAP="default" REQUIRE_PLATFORM_KEY= COLOR=true SILENT=false \
+		>>"$buildfile" 2>&1 \
+		|| cp "$buildfile" "$failfile"
+	@{ grep '\[ERRORS\]' "$buildfile" >/dev/null 2>&1 && printf "Build %-64s \e[1;31m[ERRORS]\e[0m\n" "${kb}:default" ; } \
+		|| { grep '\[WARNINGS\]' "$buildfile" >/dev/null 2>&1 && printf "Build %-64s \e[1;33m[WARNINGS]\e[0m\n" "${kb}:default" ; } \
+		|| printf "Build %-64s \e[1;32m[OK]\e[0m\n" "${kb}:default"
+	@rm -f "$buildfile" || true
+EOF
+	done
+	popd >/dev/null 2>&1
+}
+
+run_build() {
+	time make -k $MAKEFLAGS -f "$temp_makefile" -k all | tee "$qmk_builddir/build.log"
+}
+
+final_listing() {
+	num_successes=$(cat "$qmk_builddir/build.log" | egrep '\[(OK)\]' | wc -l)
+	num_skipped=$(cat "$qmk_builddir/build.log" | egrep '\[(SKIPPED)\]' | wc -l)
+	num_warnings=$(cat "$qmk_builddir/build.log" | egrep '\[(WARNINGS)\]' | wc -l)
+	num_failures=$(cat "$qmk_builddir/build.log" | egrep '\[(ERRORS)\]' | wc -l)
+	echo "-------------------------------"
+	echo "Successful builds: $num_successes"
+	echo "Skipped builds: $num_skipped"
+	echo "Warning builds: $num_warnings"
+	echo "Failing builds: $num_failures"
+	echo "-------------------------------"
+	cat "$qmk_builddir/build.log" | egrep '\[(ERRORS)\]'
+}
+
+[[ -d "$qmk_builddir" ]] || mkdir -p "$qmk_builddir"
+determine_keyboards
+generate_makefile
+run_build
+final_listing
