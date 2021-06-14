6b01fbff05db1c8b53cc284b863fe3685efc09f3
Nick Brassel <nick@tzarc.org>
2021-06-15 04:07:23 +1000

    Cleanup shiftreg test board.

--- alternates/f072_shiftreg/f072_shiftreg.c
+++ alternates/f072_shiftreg/f072_shiftreg.c
@@ -25,22 +25,19 @@ void keyboard_post_init_kb(void) {
     debug_enable   = true;
     debug_matrix   = true;
     debug_keyboard = true;
+}
 
+void matrix_init_pins(void) {
     setPinOutput(SPI_MATRIX_LATCH_PIN);
     writePinLow(SPI_MATRIX_LATCH_PIN);
     setPinOutput(SPI_MATRIX_PLOAD_PIN);
     writePinLow(SPI_MATRIX_PLOAD_PIN);
     setPinOutput(SPI_MATRIX_CHIP_SELECT_PIN);
     writePinHigh(SPI_MATRIX_CHIP_SELECT_PIN);
+    spi_init();
 }
 
 void matrix_read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row) {
-    static bool spi_initialised = false;
-    if (!spi_initialised) {
-        spi_init();
-        spi_initialised = true;
-    }
-
     if (current_row == 0) {
         // Pulse the latch pin
         writePinHigh(SPI_MATRIX_PLOAD_PIN);
@@ -54,11 +51,5 @@ void matrix_read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row)
         spi_stop();
 
         writePinLow(SPI_MATRIX_PLOAD_PIN);
-
-        static matrix_row_t last_matrix[MATRIX_ROWS] = {0xFF};
-        if (memcmp(last_matrix, current_matrix, sizeof(last_matrix)) != 0) {
-            memcpy(last_matrix, current_matrix, sizeof(last_matrix));
-            matrix_print();
-        }
     }
 }
