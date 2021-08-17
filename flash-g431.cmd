e8dc31ad9ec0cd70564daf3ec77ad220dcedf477
Nick Brassel <nick@tzarc.org>
2021-08-17 20:42:06 +1000

    Cleanup.

--- flash-g431.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-set PATH=%PATH%;E:\QMK_MSYS\mingw64\bin
-st-link_cli -q -c SWD -p qmk_firmware\.build\handwired_onekey_nucleo64_g431rb_reset.hex -HardRst
-endlocal
--- flash-onekey-proton_c.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-set PATH=%PATH%;E:\QMK_MSYS\mingw64\bin
-dfu-util.exe -a 0 -d 0483:df11 -s 0x08000000:leave -D handwired_onekey_proton_c_test_eep_rst.bin
-endlocal
