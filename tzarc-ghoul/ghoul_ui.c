242e9abb4eff218d595e4c5c62c1a0bd6cc5f3af
Nick Brassel <nick@tzarc.org>
2022-05-17 10:39:09 +1000

    Cleanup.

--- tzarc-ghoul/ghoul_ui.c
+++ tzarc-ghoul/ghoul_ui.c
@@ -90,8 +90,8 @@ void ui_task(void) {
         write_offset                = (write_offset + 1) % NUM_ADC_READS;
 
         static int counter = 0;
-        counter = (counter + 1) % 2500;
-        if(counter == 0) {
+        counter            = (counter + 1) % 2500;
+        if (counter == 0) {
             dprintf("Current: %dmA (%d) -- Voltage: %dmV (%d)\n", (int)current_ma, (int)current, (int)voltage_mv, (int)voltage);
         }
 
