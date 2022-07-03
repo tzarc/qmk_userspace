a4089fed4ad8a604618e41bb33177d9223ffa03a
Nick Brassel <nick@tzarc.org>
2022-07-04 09:52:24 +1000

    Only care about `synchronize` operations.

--- qmk_webhook_executor/runner/app/main.py
+++ qmk_webhook_executor/runner/app/main.py
@@ -19,26 +19,30 @@ async def qmk_webhook(request: Request):
     if request.headers.get('X-GitHub-Event') == 'ping':
         return {}
 
-    elif request.headers.get('X-GitHub-Event') == 'pull_request':
-        # Grab the json payload from the webhook
-        json_blob = await request.json()
+    elif request.headers.get('X-GitHub-Event') != 'pull_request':
+        return JSONResponse(content={}, status_code=403)
 
-        # Make sure we're only acting on the repository we expect
-        if json_blob['pull_request']['base']['repo']['full_name'] != expected_target_repo:
-            return JSONResponse(content={}, status_code=403)
+    # Grab the json payload from the webhook
+    json_blob = await request.json()
 
-        # Set up the arguments we want to use
-        invoke_args = {
-            'target_repo': json_blob['pull_request']['base']['repo']['clone_url'],
-            'target_branch': json_blob['pull_request']['base']['ref'],
-            'pr_num': json_blob['number'],
-            'sha1': json_blob['pull_request']['head']['sha'],
-            'author': json_blob['pull_request']['user']['login'],
-            'title': json_blob['pull_request']['title']
-        }
+    # Make sure we're only acting on the repository we expect
+    if json_blob['pull_request']['base']['repo']['full_name'] != expected_target_repo:
+        return JSONResponse(content={}, status_code=403)
 
-        # Queue the work unit and exit
-        q.enqueue(execute_run, ttl=(86400*3), kwargs=invoke_args)
-        return invoke_args
+    if json_blob['action'] != 'synchronize':
+        return {}
 
-    return JSONResponse(content={}, status_code=403)
+    # Set up the arguments we want to use
+    invoke_args = {
+        'target_repo': json_blob['pull_request']['base']['repo']['clone_url'],
+        'target_branch': json_blob['pull_request']['base']['ref'],
+        'pr_num': json_blob['number'],
+        'sha1': json_blob['pull_request']['head']['sha'],
+        'author': json_blob['pull_request']['user']['login'],
+        'title': json_blob['pull_request']['title']
+    }
+
+    # Queue the work unit and exit
+    q.enqueue(execute_run, ttl=(86400*3), kwargs=invoke_args)
+    return {}
+
