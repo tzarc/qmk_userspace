a1a6b1243d9e9d8edf9dbb3be1c9242fb3c1fa78
Nick Brassel <nick@tzarc.org>
2022-08-06 21:02:41 +1000

    Fixup permissions.

--- qmk_develop_builder/create-builder.sh
+++ qmk_develop_builder/create-builder.sh
@@ -4,5 +4,9 @@ this_script="$(readlink -f "${BASH_SOURCE[0]}")"
 script_dir="$(readlink -f "$(dirname "$this_script")")"
 
 docker rm qmk_develop_runner || true
+
+rm "$script_dir/.repo-hash"
 echo > "$script_dir/.repo-hash"
+chmod 666 "$script_dir/.repo-hash"
+
 renc -i "$HOME/.ssh/id_ed25519" < "$script_dir/create-builder.sh.enc" | bash
