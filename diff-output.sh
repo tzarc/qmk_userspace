517f129c116afce7f7f1eb0a567c0f2f8968c797
Nick Brassel <nick@tzarc.org>
2023-03-30 09:17:08 +1100

    Fixup delta frames.

---
+++ diff-output.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+base_rev=$1
+target_rev=$2
+
+added_files() {
+    {
+        diff --suppress-common-lines <(git ls-tree -r --name-only $base_rev) <(git ls-tree -r --name-only $target_rev) | awk '/^[>]/ {print $2}'
+        git diff --name-status $base_rev $target_rev | awk '/^[R]/ {print $4}'
+    } | sort | uniq
+}
+
+removed_files() {
+    {
+        diff --suppress-common-lines <(git ls-tree -r --name-only $base_rev) <(git ls-tree -r --name-only $target_rev) | awk '/^[<]/ {print $2}'
+        git diff --name-status $base_rev $target_rev | awk '/^[R]/ {print $2}'
+    } | sort | uniq
+}
+
+modified_files() {
+    git diff --name-status $base_rev $target_rev | awk '/^[M]/ {print $2}'
+}
+
+echo -e "\e[38;5;10mAdded files:\e[0m"
+added_files
+echo -e "\e[38;5;9mRemoved files:\e[0m"
+removed_files
+echo -e "\e[38;5;11mModified files:\e[0m"
+modified_files
