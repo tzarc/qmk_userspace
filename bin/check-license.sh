9d8fa7b9f4f49fe7c7172dc365f95443690c6bd0
Nick Brassel <nick@tzarc.org>
2024-02-10 23:22:59 +1100

    Only change license year on first 5 lines.

--- bin/check-license.sh
+++ bin/check-license.sh
@@ -19,8 +19,9 @@ git ls-files | grep '\(Makefile.*\|Dockerfile.*\|\.\(c\|cpp\|h\|hpp\|mk\|sh\|py\
 done
 
 git grep '\(#\|//\|::\) Copyright.*tzarc' | cut -d: -f1 | while read file ; do
-    sed -i 's%^\(#\|//\|::\) Copyright.*tzarc.*$%\1 Copyright 2018-'$(date +%Y)' Nick Brassel (@tzarc)%g' $file
-    sed -i 's%^\(#\|//\|::\) SPDX-License-Identifier.*$%\1 SPDX-License-Identifier: GPL-2.0-or-later%g' $file
+    # Limit license year changes to first 5 lines of the file
+    sed -i '1,5s%^\(#\|//\|::\) Copyright.*tzarc.*$%\1 Copyright 2018-'$(date +%Y)' Nick Brassel (@tzarc)%g' $file
+    sed -i '1,5s%^\(#\|//\|::\) SPDX-License-Identifier.*$%\1 SPDX-License-Identifier: GPL-2.0-or-later%g' $file
 done
 
 exit $exitcode
