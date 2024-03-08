5ad3012f936b1dff4ce4196dfd413980fcbac6b3
Nick Brassel <nick@tzarc.org>
2024-03-09 04:10:58 +1100

    More toolchain testing.

--- bin/deploy-toolchains.sh
+++ bin/deploy-toolchains.sh
@@ -2,11 +2,10 @@
 # Copyright 2018-2024 Nick Brassel (@tzarc)
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+set -eEuo pipefail
+
 function fn_os() {
-    local os_name=$(echo ${1:-} | tr 'A-Z' 'a-z')
-    if [[ -z "$os_name" ]]; then
-        os_name=$(uname -s | tr 'A-Z' 'a-z')
-    fi
+    local os_name=$(uname -s | tr 'A-Z' 'a-z')
     case "$os_name" in
     *darwin* | *macos*)
         echo macos
@@ -17,47 +16,42 @@ function fn_os() {
     *linux*)
         echo linux
         ;;
-    *none* | *unknown* | *picolibc* | *nano*)
-        echo baremetal
-        ;;
     *)
-        echo unknown
+        echo "unknown operating system '$os_name'" >&2
+        exit 1
         ;;
     esac
 }
 
 function fn_arch() {
-    local arch_name=$(echo ${1:-} | tr 'A-Z' 'a-z')
-    if [[ -z "$arch_name" ]]; then
-        arch_name=$(uname -m | tr 'A-Z' 'a-z')
-    fi
+    local arch_name=$(uname -m | tr 'A-Z' 'a-z')
     case "$arch_name" in
     *arm64* | *aarch64*)
         echo ARM64
-        ;;
-    *arm*)
-        echo ARM
-        ;;
-    *riscv32*)
-        echo RV32
-        ;;
-    *avr*)
-        echo AVR
         ;;
     *x86_64* | *x64*)
         echo X64
         ;;
     *)
-        echo unknown
+        echo unknown architecture $arch_name >&2
+        exit 1
         ;;
     esac
 }
 
-echo OS: $(fn_os), Arch: $(fn_arch)
+tag_name=${1:-latest}
 
-[[ -f qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalARM.tar.xz ]] || curl -fsSLO https://github.com/tzarc/qmk_toolchains/releases/download/latest/qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalARM.tar.xz
-[[ -f qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalAVR.tar.xz ]] || curl -fsSLO https://github.com/tzarc/qmk_toolchains/releases/download/latest/qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalAVR.tar.xz
-[[ -f qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalRV32.tar.xz ]] || curl -fsSLO https://github.com/tzarc/qmk_toolchains/releases/download/latest/qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalRV32.tar.xz
+echo OS: $(fn_os), Arch: $(fn_arch), tag: ${tag_name}
+
+[[ -f qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalARM.tar.xz ]] ||
+    echo "Downloading ARM toolchain" &&
+    curl -fsSLO https://github.com/tzarc/qmk_toolchains/releases/download/${tag_name}/qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalARM.tar.xz
+[[ -f qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalAVR.tar.xz ]] ||
+    echo "Downloading AVR toolchain" &&
+    curl -fsSLO https://github.com/tzarc/qmk_toolchains/releases/download/${tag_name}/qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalAVR.tar.xz
+[[ -f qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalRV32.tar.xz ]] ||
+    echo "Downloading RISC-V toolchain" &&
+    curl -fsSLO https://github.com/tzarc/qmk_toolchains/releases/download/${tag_name}/qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalRV32.tar.xz
 
 if [[ -d "$HOME/.local/qmk/toolchains" ]]; then
     rm -rf "$HOME/.local/qmk/toolchains"
@@ -65,6 +59,17 @@ fi
 
 mkdir -p "$HOME/.local/qmk/toolchains"
 
-tar -C "$HOME/.local/qmk/toolchains" -xf qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalARM.tar.xz --strip-components=1
-tar -C "$HOME/.local/qmk/toolchains" -xf qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalAVR.tar.xz --strip-components=1
-tar -C "$HOME/.local/qmk/toolchains" -xf qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalRV32.tar.xz --strip-components=1
+echo "Extracting ARM toolchain" &&
+    tar -C "$HOME/.local/qmk/toolchains" -xf qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalARM.tar.xz --strip-components=1
+echo "Extracting AVR toolchain" &&
+    tar -C "$HOME/.local/qmk/toolchains" -xf qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalAVR.tar.xz --strip-components=1
+echo "Extracting RISC-V toolchain" &&
+    tar -C "$HOME/.local/qmk/toolchains" -xf qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_baremetalRV32.tar.xz --strip-components=1
+
+if [[ $(fn_os) == "linux" ]]; then
+    [[ -f qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_$(fn_os)$(fn_arch).tar.xz ]] ||
+        echo "Downloading $(fn_os)$(fn_arch) toolchain" &&
+        curl -fsSLO https://github.com/tzarc/qmk_toolchains/releases/download/${tag_name}/qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_$(fn_os)$(fn_arch).tar.xz
+    echo "Extracting $(fn_os)$(fn_arch) toolchain" &&
+        tar -C "$HOME/.local/qmk/toolchains" -xf qmk_toolchain-gcc13.2.0-host_$(fn_os)$(fn_arch)-target_$(fn_os)$(fn_arch).tar.xz --strip-components=1
+fi
---
+++ bin/test-toolchains.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright 2018-2024 Nick Brassel (@tzarc)
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+set -eEuo pipefail
+
+this_script=$(realpath "${BASH_SOURCE[0]}")
+script_dir=$(dirname "$this_script")
+cd $script_dir/..
+
+test_distro() {
+    local distro=$1
+    echo -e '\e[38;5;11m################################################################\e[0m'
+    echo -e "\e[38;5;6mTesting: \e[38;5;14m$distro\e[0m"
+    echo -e '\e[38;5;11m################################################################\e[0m'
+    docker run --rm -it -v $HOME/.local/qmk/toolchains:/opt/qmk $distro bash -c "export PATH=\"/opt/qmk/bin:\$PATH\"; avr-gcc -v && arm-none-eabi-gcc -v && riscv32-unknown-elf-gcc -v"
+}
+
+test_distro debian:12
+test_distro debian:11
+test_distro debian:10
+
+test_distro fedora:36
+test_distro fedora:37
+test_distro fedora:38
+test_distro fedora:39
+
+test_distro archlinux:latest
