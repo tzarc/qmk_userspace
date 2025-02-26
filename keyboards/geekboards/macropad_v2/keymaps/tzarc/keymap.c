3bad870653879bf9605ec3e9edf12c5c47bae7b2
Nick Brassel <nick@tzarc.org>
2025-02-26 23:53:26 +1100

    Updates.

--- keyboards/geekboards/macropad_v2/keymaps/tzarc/keymap.c
+++ keyboards/geekboards/macropad_v2/keymaps/tzarc/keymap.c
@@ -2,33 +2,13 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
 #include "tzarc.h"
-
-enum {
-    RE_1 = KEYMAP_SAFE_RANGE,
-    RE_50,
-};
+#include "tzarc_layout.h"
 
 // clang-format off
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT_ortho_2x4(
-        QK_BOOT, RGB_MOD, RE_1, RE_50,
-        KC_5,    KC_6, KC_7, KC_8
+        KC_GLOBE, XRGB_NXT, XRGB_PRV, QK_BOOT,
+        KC_C,     KC_F,     KC_N,     KC_Q
     )
 };
 // clang-format on
-
-bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case RE_1:
-            if (record->event.pressed) {
-                rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
-            }
-            break;
-        case RE_50:
-            if (record->event.pressed) {
-                rgb_matrix_mode(RGB_MATRIX_CUSTOM_rv32_effect);
-            }
-            break;
-    }
-    return true;
-}
