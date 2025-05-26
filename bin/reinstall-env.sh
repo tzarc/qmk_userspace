f9ef9236209c6ad63a0fe1b7f5f8e18e1f7bf4df
Nick Brassel <nick@tzarc.org>
2025-05-26 22:07:53 +1000

    Updated env installation.

--- bin/reinstall-env.sh
+++ bin/reinstall-env.sh
@@ -3,6 +3,6 @@
 this_script=$(realpath "${BASH_SOURCE[0]}")
 script_dir=$(dirname "$this_script")
 
-curl -fsSL https://install.qmk.fm/ | CONFIRM=1 SKIP_PACKAGE_MANAGER=1 SKIP_UV=1 SKIP_QMK_CLI=1 sh
+curl -fsSL https://install.qmk.fm/ | CONFIRM=1 SKIP_PACKAGE_MANAGER=1 SKIP_UV=1 SKIP_UDEV_RULES=1 sh
 
 "${script_dir}/post-checkout.sh"
